17c7b6c75b3140de87e149daee44336e26e6a297
Vedant Chainani <vedantchainani1084@gmail.com>
2024-08-12 18:28:18 +0530

    chore: add basic npm package boilerplate

---
+++ packages/nillion-key-manager/src/webauthn/index.ts
@@ -0,0 +1,3 @@
+export const webauthn = () => {
+  console.log('webauthn');
+};
---
+++ packages/nillion-key-manager/tsup.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, type Options } from 'tsup';
+
+const commonConfig: Options = {
+  treeshake: true,
+  sourcemap: true,
+  minify: true,
+  shims: true,
+  format: ['cjs', 'esm'],
+  dts: true,
+  splitting: false,
+  clean: true,
+  target: ['es6', 'es2022'],
+  tsconfig: 'tsconfig.json',
+};
+
+export default defineConfig([
+  {
+    entry: ['./src/webauthn/index.ts'],
+    outDir: 'dist/webauthn',
+    ...commonConfig,
+  },
+  {
+    entry: ['./src/snap/index.ts'],
+    outDir: 'dist/snap',
+    ...commonConfig,
+  },
+]);
