874d0a3d8d63bda1d07acea6c4a77ecc67c88f3f
Vedant Chainani <vedantchainani1084@gmail.com>
2024-08-17 21:42:05 +0530

    fix: metadata

--- apps/www/src/app/(home)/layout.tsx
+++ apps/www/src/app/(home)/layout.tsx
@@ -1,29 +1,10 @@
 import type { ReactNode } from 'react';
 
 import { baseOptions } from '~/lib/fumadocs';
-import { baseUrl, createMetadata } from '~/lib/metadata';
 
 import { HomeLayout } from 'fumadocs-ui/home-layout';
-import type { Viewport } from 'next';
 
 import { Footer } from '~/components/fumadocs';
-
-export const metadata = createMetadata({
-  title: {
-    template: '%s | Nillion Tools',
-    default: 'Nillion Tools',
-  },
-  description:
-    'Build blazing-fast apps on Nillion with our developer-friendly toolkit. Simple API, seamless integrations. 🚀',
-  metadataBase: baseUrl,
-});
-
-export const viewport: Viewport = {
-  themeColor: [
-    { media: '(prefers-color-scheme: dark)', color: '#0A0A0A' },
-    { media: '(prefers-color-scheme: light)', color: '#fff' },
-  ],
-};
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
