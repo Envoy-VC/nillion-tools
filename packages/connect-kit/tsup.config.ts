c2d442a4ac4d753fc374943c659fbe890016492b
Vedant Chainani <vedantchainani1084@gmail.com>
2024-08-19 23:39:02 +0530

    chore: add basic connect-kit template

---
+++ packages/connect-kit/tsup.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig, type Options } from 'tsup';
+
+const commonConfig: Options = {
+  treeshake: true,
+  sourcemap: true,
+  minify: true,
+  shims: true,
+  format: ['cjs', 'esm'],
+  dts: true,
+  splitting: false,
+  clean: true,
+  target: ['es6', 'es2022'],
+  tsconfig: 'tsconfig.json',
+  banner: {
+    js: "'use client';",
+  },
+  injectStyle: true,
+  external: ['react'],
+};
+
+export default defineConfig([
+  {
+    entry: ['./src/index.ts'],
+    outDir: 'dist/',
+    ...commonConfig,
+  },
+]);
