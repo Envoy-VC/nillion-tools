f38d51277c6ff474c99e20079ef3443fb20c5c0e
Vedant Chainani <vedantchainani1084@gmail.com>
2024-09-08 22:40:30 +0530

    chore: add typedoc config

--- packages/connect-kit/src/index.tsx
+++ packages/connect-kit/src/index.tsx
@@ -5,7 +5,11 @@ export { useConnectWallet, useUser } from './lib/hooks';
 export * from './components';
 export * from './lib/chain';
 export type {
+  ButtonOptions,
+  ModalOptions,
   PartialConnectWalletProps as ConnectWalletProps,
   WalletType,
   Screen,
 } from './types';
+
+export * from './types/wallet';
--- packages/connect-kit/src/types/index.ts
+++ packages/connect-kit/src/types/index.ts
@@ -1,13 +1,19 @@
 import type { ReactNode, CSSProperties } from 'react';
 import type { supportedWallets } from '~/providers';
 
-type WelcomeScreen =
-  | {
-      img?: { height?: number; src: string; width?: number };
-      subtitle?: string;
-      title?: string;
-    }
-  | (() => React.ReactNode);
+export interface ButtonOptions {
+  label?: string;
+  className?: string;
+  style?: CSSProperties;
+}
+
+export interface ModalOptions {
+  size?: 'compact' | 'wide';
+  privacyPolicyUrl?: string;
+  termsOfServiceUrl?: string;
+  title?: ReactNode;
+  welcomeScreen?: React.ReactNode;
+}
 
 export interface ConnectWalletProps {
   mode: 'inline' | 'modal';
@@ -27,7 +33,7 @@ export interface ConnectWalletProps {
     privacyPolicyUrl?: string;
     termsOfServiceUrl?: string;
     title: ReactNode;
-    welcomeScreen?: WelcomeScreen;
+    welcomeScreen?: React.ReactNode;
   };
 }
 
@@ -42,21 +48,7 @@ export type Screen =
 export interface PartialConnectWalletProps {
   mode?: 'inline' | 'modal';
   chain?: string;
-  signInButton?: {
-    label?: string;
-    className?: string;
-    style?: CSSProperties;
-  };
-  switchButton?: {
-    className?: string;
-    label?: string;
-    style?: React.CSSProperties;
-  };
-  modalOptions?: {
-    size?: 'compact' | 'wide';
-    privacyPolicyUrl?: string;
-    termsOfServiceUrl?: string;
-    title?: ReactNode;
-    welcomeScreen?: WelcomeScreen;
-  };
+  signInButton?: ButtonOptions;
+  switchButton?: ButtonOptions;
+  modalOptions?: ModalOptions;
 }
